5c1bd18ed2fc4ce5dff628b2f5c68c4afbdabba3
norude <fedor.tryfanau@gmail.com>
2025-08-12 21:21:34 +0300

    first steps

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "tts",
+    fullname = _("text to speech plugin <DEV>"),
+    description = _([[This is some day gonna be a working tts plugin]]),
+}
