a8319f5a1f2c46a1573ae707004f603a0f3c1c44
norude <fedor.tryfanau@gmail.com>
2025-08-17 21:30:57 +0300

    MASSIVE bugfixes, moved the wav request into lua

--- main.lua
+++ main.lua
@@ -1,53 +1,69 @@
-local EventListener = require("ui/widget/eventlistener")
 local Blitbuffer = require("ffi/blitbuffer")
-local InputContainer = require("ui/widget/container/inputcontainer")
 local ButtonTable = require("ui/widget/buttontable")
 local DataStorage = require("datastorage")
 local Dbg = require("dbg")
 local Device = require("device")
 local Dispatcher = require("dispatcher")
 local Event = require("ui/event")
+local EventListener = require("ui/widget/eventlistener")
 local FrameContainer = require("ui/widget/container/framecontainer")
 local ImageWidget = require("ui/widget/imagewidget")
 local InfoMessage = require("ui/widget/infomessage")
+local InputContainer = require("ui/widget/container/inputcontainer")
 local LuaSettings = require("luasettings")
+local Screen = Device.screen
 local Size = require("ui/size")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
-local logger = require("logger")
-local util = require("util")
-local ffiutil = require("ffi/util")
 local _ = require("gettext")
-local Screen = Device.screen
--- Dbg:turnOn()
+local ffiutil = require("ffi/util")
+local http = require("socket.http")
+local logger = require("logger")
+local ltn12 = require("ltn12")
+local rapidjson = require("rapidjson")
+local util = require("util")
+Dbg:turnOn()
 ---@class TTS:WidgetContainer
 local TTS = WidgetContainer:extend({
 	name = "name of tts widget",
 	fullname = _("fullname of tts widget"),
 	settings = nil, -- nil means uninit widget
+	luasettings = nil, -- nil means uninit widget
 	prev_item = nil, -- nil means current_item is the first possible
 	next_item = nil, -- nil means current_item is the last possible
 	current_item = nil, -- nil means tts is not started
 	current_highlight_idx = nil, -- nil means tts is not started
 	widget = nil, -- nil means tts is not started
-	highlight_style = {}, -- means uninit
 	playing_promise = nil, -- nil means not playing rn
+	highlight_style = {}, -- means uninit
 })
 
+---@type ReaderUI
+TTS.ui = nil
+---@type ReaderView
+TTS.view = nil
 function TTS:init()
-	if not self.settings then
+	if not self.luasettings then
 		self:readSettingsFile()
 	end
 	self.ui.menu:registerToMainMenu(self)
 end
 
 function TTS:readSettingsFile()
-	self.settings = LuaSettings:open(DataStorage:getSettingsDir() .. "/tts.lua")
-	local highlight_style = self.settings:child("highlight_style")
-	self.highlight_style.drawer = highlight_style:readSetting("drawer", "lighten")
-	self.highlight_style.color = highlight_style:readSetting("color", "gray")
+	self.luasettings = LuaSettings:open(DataStorage:getSettingsDir() .. "/tts.lua")
+	self.settings = {}
+	self.settings.drawer = self.luasettings:readSetting("highlight_style.drawer", "lighten")
+	self.settings.color = self.luasettings:readSetting("color", "gray")
+	self.settings.always_follow = self.luasettings:readSetting("always_follow", true)
+	self.settings.hostname = self.luasettings:readSetting("hostname", "localhost:5000")
+	self.settings.server_extra_args = self.luasettings:readSetting("server_extra_args", {
+		length_scale = 1,
+	})
+	self:settings_flush()
+end
 
-	self.settings:flush()
+function TTS:settings_flush()
+	self.luasettings:flush()
 end
 
 function TTS:onCloseDocument()
@@ -59,21 +75,21 @@ end
 
 function TTS:onCloseWidget()
 	logger.warn("TTS: onCloseWidget")
-	self.settings:flush()
+	self:settings_flush()
 end
 
 function TTS:addToMainMenu(menu_items)
 	menu_items.tts = {
-		text = _("TTS stuff"),
 		sorting_hint = "main",
-		sub_item_table = {
-			{
-				text = _("Start TTS"),
-				callback = function()
-					self:start_tts_mode()
-				end,
-			},
-		},
+		-- text = _("TTS stuff"),
+		-- sub_item_table = {
+		-- 	{
+		text = _("Start TTS Mode"),
+		callback = function()
+			self:start_tts_mode()
+		end,
+		-- },
+		-- },
 	}
 end
 
@@ -99,6 +115,7 @@ function TTS:create_highlight()
 	-- flush
 	self.view.footer:maybeUpdateFooter()
 	self.ui:handleEvent(Event:new("AnnotationsModified", { item, nb_highlights_added = 1, index_modified = index }))
+	self.ui:handleEvent(Event:new("GotoXPointer", self.current_item.pos0))
 end
 
 function TTS:change_highlight(item)
@@ -107,9 +124,14 @@ function TTS:change_highlight(item)
 		Event:new("AnnotationsModified", { item, self.current_item, index_modified = self.current_highlight_idx })
 	)
 	UIManager:setDirty(self.dialog, "ui")
+	logger.info("highlighting the ", item)
 	self.current_item = item
-	if not self.ui.document:isXPointerInCurrentPage(item.pos0) then
-		self.ui:handleEvent(Event:new("GotoXPointer", item.pos0))
+	if
+		self.settings.always_follow
+		or not self.ui.document:isXPointerInCurrentPage(self.current_item.pos0)
+		or not self.ui.document:isXPointerInCurrentPage(self.current_item.pos1)
+	then
+		self.ui:handleEvent(Event:new("GotoXPointer", self.current_item.pos0))
 	end
 end
 
@@ -121,16 +143,25 @@ function TTS:remove_highlight()
 end
 
 function TTS:xpointer_start(xpointer)
-	-- TODO: if xpointer and xpointer_start(xpointer) are on diffrent pages, set xpointer_start(xpointer) to the start of the page instead
+	local wrap = self.ui.document.configurable.text_wrap
+	self.ui.document.configurable.text_wrap = 0
+	local page = self.ui.document:getPageFromXPointer(xpointer)
 	local prefix = xpointer:match("^(.*)%.[^%.]*$")
 	if not prefix then
 		prefix = xpointer
 	end
-	return prefix .. ".0"
+	local start = prefix .. ".0"
+	if self.view.view_mode == "page" and page ~= self.ui.document:getPageFromXPointer(start) then
+		return self.ui.document:getPageXPointer(page)
+	end
+	self.ui.document.configurable.text_wrap = wrap
+	return start
 end
 
 function TTS:xpointer_end(xpointer)
-	-- TODO: if xpointer and xpointer_end(xpointer) are on diffrent pages, set xpointer_end(xpointer) to the end of the page instead
+	local wrap = self.ui.document.configurable.text_wrap
+	self.ui.document.configurable.text_wrap = 0
+	local page = self.ui.document:getPageFromXPointer(xpointer)
 	local prefix = xpointer:match("^(.*)%.[^%.]*$")
 	if not prefix then
 		prefix = xpointer
@@ -138,26 +169,24 @@ function TTS:xpointer_end(xpointer)
 	prefix = prefix .. "."
 	local text = self.ui.document:getTextFromXPointer(xpointer)
 	local text_len = text:len()
-	local idx = text_len
-	-- This is VERY hacky, but the xpointer at prefix..text_len overshoots, so we just find the last one that works
-	-- FIXME: This is a task for a binary search
-	while select("#", self.ui.document:getTextFromXPointer(prefix .. idx)) == 0 do
-		if idx < 0 then -- Hopefully will never trigger
-			idx = text_len * 2
+	local max = text_len * 2
+	local min = 0
+	while min < max do
+		local mid = math.floor((min + max) / 2)
+		logger.warn("Guessed ", mid, "max is ", max, " min is", min)
+		if
+			select("#", self.ui.document:getTextFromXPointer(prefix .. mid)) ~= 0
+			and (self.view.view_mode ~= "page" or page >= self.ui.document:getPageFromXPointer(prefix .. mid))
+		then
+			logger.warn("was correct")
+			min = mid + 1
+		else
+			logger.warn("was too high")
+			max = mid
 		end
-		idx = idx - 1
 	end
-	logger.warn(
-		"Found the end of the xpointer. \n\n\n Text_len was ",
-		text_len,
-		"But actual is",
-		idx,
-		"\n\t THE DIFFRENCE IS ",
-		text_len - idx,
-		"\n For reference, this is the text:\n",
-		text
-	)
-	return prefix .. idx
+	self.ui.document.configurable.text_wrap = wrap
+	return prefix .. min - 1
 end
 
 function TTS:item_from_xpointer(xpointer)
@@ -165,26 +194,14 @@ function TTS:item_from_xpointer(xpointer)
 		pos0 = self:xpointer_start(xpointer),
 		pos1 = self:xpointer_end(xpointer),
 	}
-	local pg_or_xp
-	if self.ui.rolling then
-		local extended_text = self.ui.document:extendXPointersToSentenceSegment(selected_text.pos0, selected_text.pos1)
-		if extended_text then
-			logger.warn("EXTENDED TXT ACTUALLY WORKED. THIS IS IT:", extended_text, "\nIT WAS", selected_text, "\n\n\n")
-			selected_text = extended_text
-		end
-		pg_or_xp = selected_text.pos0
-	else
-		pg_or_xp = selected_text.pos0.page
-	end
-	local text = self.ui.document:getTextFromXPointer(selected_text.pos0)
+	local text = self.ui.document:getTextFromXPointers(selected_text.pos0, selected_text.pos1)
 	local item = {
 		page = self.ui.paging and selected_text.pos0.page or selected_text.pos0,
 		pos0 = selected_text.pos0,
 		pos1 = selected_text.pos1,
 		text = util.cleanupSelectedText(text),
-		drawer = self.highlight_style.drawer,
-		color = self.highlight_style.color,
-		chapter = self.ui.toc:getTocTitleByPage(pg_or_xp),
+		drawer = self.settings.drawer,
+		color = self.settings.color,
 	}
 	return item
 end
@@ -232,7 +249,7 @@ function TTS:show_widget()
 					{
 						text = "nothing",
 						callback = function()
-							logger.warn("clicked on nothing")
+							logger.warn("nothing")
 						end,
 					},
 					{
@@ -241,7 +258,7 @@ function TTS:show_widget()
 							local was_playing = self.playing_promise ~= nil
 							self:stop_playing()
 							if self.next_item ~= nil and self.next_item.wav_promise ~= nil then
-								self.next_item.wav_promise:cancel()
+								self.next_item.wav_promise:cancel() -- FIXME: we need to actually kill someone here
 							end
 							self.next_item = self.current_item
 							self:change_highlight(self.prev_item or self.current_item)
@@ -274,7 +291,7 @@ function TTS:show_widget()
 							local was_playing = self.playing_promise ~= nil
 							self:stop_playing()
 							if self.prev_item ~= nil and self.prev_item.wav_promise ~= nil then
-								self.prev_item.wav_promise:cancel()
+								self.prev_item.wav_promise:cancel() -- FIXME: we need to actually kill someone here
 							end
 							self.prev_item = self.current_item
 							self:change_highlight(self.next_item or self.current_item)
@@ -307,64 +324,25 @@ end
 
 ---------------- simple promises like in JS because we are doing some async and I don't know better ----------
 
-local pending = { "PENDING" }
-local finished = { "FINISHED" }
-local canceled = { "CANCELED" }
 ---@class Promise:EventListener
 local Promise = EventListener:extend({
-	state = pending,
 	callbacks = nil, -- nil means resolved, an array (even empty) means pending
+	on_cancel = nil,
 })
 
 function Promise:resolve()
-	logger.warn("resolving on this promise: ", self, "\n")
-	Dbg:traceback()
-	if self.callbacks == nil or self.state ~= pending then
+	if self.callbacks == nil then
 		return
 	end
 	for i, callback in ipairs(self.callbacks) do
-		logger.warn("resolving callback #", i)
 		callback()
 	end
 	self.callbacks = nil
-	self.state = finished
-	-- local run_callback
-	-- run_callback = function(idx)
-	-- 	if self.callbacks == nil then
-	-- 		return
-	-- 	end
-	-- 	local callback = self.callbacks[idx]
-	-- 	if callback == nil then
-	-- 		self.callbacks = nil
-	-- 		return
-	-- 	end
-	-- 	---@type Promise?
-	-- 	local p = callback()
-	-- 	if p ~= nil and p.callbacks ~= nil then
-	-- 		UIManager:nextTick(function()
-	-- 			p:and_then(function()
-	-- 				run_callback(idx + 1)
-	-- 			end)
-	-- 		end)
-	-- 		return
-	-- 	end
-	-- 	UIManager:nextTick(function()
-	-- 		run_callback(idx + 1)
-	-- 	end)
-	-- end
-	--
-	-- UIManager:nextTick(function()
-	-- 	run_callback(1)
-	-- end)
 end
 
 ---@param callback fun()--:Promise?
 function Promise:and_then(callback)
-	logger.warn("called and_then on this promise:", self, "\n\n")
-	if self.state == canceled then
-		return self
-	end
-	if self.state == finished then
+	if self.callbacks == nil then
 		return callback() or self
 	end
 	self.callbacks[#self.callbacks + 1] = callback
@@ -372,12 +350,15 @@ function Promise:and_then(callback)
 end
 
 function Promise:cancel()
-	self.state = canceled
 	self.callbacks = nil
-end
-
-function Promise:is_pending()
-	return self.state == pending
+	self.and_then = function()
+		return self
+	end
+	self.resolve = function() end
+	if self.on_cancel ~= nil then
+		self:on_cancel()
+	end
+	self.on_cancel = nil
 end
 
 function Promise.wait_while(condition)
@@ -395,29 +376,16 @@ function Promise.wait_while(condition)
 end
 
 function Promise.instant()
-	return Promise:new({ callbacks = {}, state = finished })
+	return Promise:new({ callbacks = nil })
 end
 
 function Promise.empty()
-	return Promise:new({ callbacks = {}, state = pending })
+	return Promise:new({ callbacks = {} })
 end
-
------@param other Promise
--- function Promise:join(other)
--- 	local promise = Promise.empty()
--- 	self:and_then(function()
--- 		other:and_then(function()
--- 			promise:resolve()
--- 		end)
--- 	end)
--- 	return promise
--- end
 
 function Promise:wrap()
 	local promise = Promise.empty()
-	promise.WRAP_MARK = self
 	self:and_then(function()
-		logger.warn("resolving going outer")
 		promise:resolve()
 	end)
 	return promise
@@ -427,32 +395,45 @@ end
 
 function TTS:play(item)
 	Dbg.dassert(item.wav_promise == nil and item.wav ~= nil, "tried to play an item before creating the wav file")
-	logger.warn("playing item", item)
+	-- logger.warn("playing item", item)
 	local process = io.popen("plugins/TTS.koplugin/play " .. item.wav, "r")
 	if process == nil then
 		return
 	end
 	local promise = Promise.empty()
 	promise = Promise.wait_while(function()
-		local a = ffiutil.getNonBlockingReadSize(process)
-		logger.err(a)
-		return promise:is_pending() and a == 0
-	end):and_then(function()
-		logger.warn("playing endend, promise is ", promise)
-		-- local _ = process:read("*a")
-		-- process:close()
+		return promise.callbacks ~= nil and ffiutil.getNonBlockingReadSize(process) == 0
 	end)
-	promise.MARK0 = "play watcher"
-	promise = promise:wrap()
+		:and_then(function()
+			local _ = process:read("*a")
+			process:close()
+		end)
+		:wrap()
 
-	self.playing_promise = promise
-	self.playing_promise.MARK = "the tts:play monitoring promise"
+	promise.on_cancel = function()
+		os.execute("plugins/TTS.koplugin/stop_playing")
+	end
 	return promise
 end
 
-function TTS:stop_tts_server() end
-
 function TTS:start_tts_server() end
+
+function TTS:request_server(body)
+	local result = {}
+	local _, code = http.request({
+		method = "POST",
+		url = "http://" .. self.settings.hostname,
+		source = ltn12.source.string(body),
+		headers = {
+			["Content-Length"] = #body,
+			["Content-Type"] = "application/json",
+		},
+		sink = ltn12.sink.table(result),
+	})
+	return code, result
+end
+
+function TTS:stop_tts_server() end
 
 ---@return Promise
 function TTS:ensure_wav_on_item(item, wav_name)
@@ -464,40 +445,41 @@ function TTS:ensure_wav_on_item(item, wav_name)
 	end
 
 	item.wav = wav_name
-	local sub = function()
-		local process = io.popen("plugins/TTS.koplugin/create_wav " .. wav_name, "w")
-		if process == nil then
-			return
+	local download_thread = function()
+		local body = rapidjson.encode({
+			text = item.text,
+		})
+		body = self.extend(body, self.settings.server_extra_args)
+		local code, wav_table = self:request_server(body)
+		if code == 200 then
+			ltn12.pump.all(
+				ltn12.source.table(wav_table),
+				ltn12.sink.file(io.open("plugins/TTS.koplugin/" .. wav_name, "w"))
+			)
 		end
-		process:write(item.text)
-		process:close() -- this is blocking, but we need it to write EOF for the script to start, so I just ran it in a separate thread
-		-- TODO: try to resolve from here
 	end
-	local pid = ffiutil.runInSubProcess(sub)
+	local pid = ffiutil.runInSubProcess(download_thread)
 	local promise = Promise.wait_while(function()
 		return not ffiutil.isSubProcessDone(pid)
-	end):and_then(function()
-		item.wav_promise = nil
-		logger.warn("ENDED WAV GENERATION, c is ", promise, "\n\n")
 	end)
-
-	promise.MARK0 = "ensure wav watcher"
-	promise = promise:wrap()
+		:and_then(function()
+			item.wav_promise = nil
+		end)
+		:wrap()
+	promise.on_cancel = function()
+		ffiutil.terminateSubProcess(pid)
+	end
 	item.wav_promise = promise
-	promise.MARK = "TTS:ENSURE_WAV_ON_ITEM"
 	return promise
 end
 
 function TTS:stop_playing()
 	if self.playing_promise ~= nil then
-		logger.warn("current promise is ", self.playing_promise, " CANCELING IT\n\n")
+		-- logger.warn("current promise is ", self.playing_promise, " CANCELING IT\n\n")
 		self.playing_promise:cancel()
 		self.playing_promise = nil
-		local process = os.execute("plugins/TTS.koplugin/stop_playing")
 	end
 end
-
-
 
 function TTS:start_playing()
 	local choose_name = function()
@@ -520,7 +502,6 @@ function TTS:start_playing()
 
 	local play_once
 	play_once = function()
-		logger.warn("starting playing")
 		if self.next_item == nil then
 			-- We hit the end of the book in tts mode
 			self:stop_playing()
@@ -529,16 +510,15 @@ function TTS:start_playing()
 			return
 		end
 		local wav_for_the_next = self:ensure_wav_on_item(self.next_item, choose_name())
-		self:play(self.current_item):and_then(function()
-			logger.err("DONE!!!!!!")
-			-- self.prev_item = self.current_item
-			-- self:change_highlight(self.next_item)
-			-- self.next_item = self:item_next(self.next_item)
-			-- wav_for_the_next:and_then(play_once)
+		self.playing_promise = self:play(self.current_item)
+		self.playing_promise:and_then(function()
+			self.prev_item = self.current_item
+			self:change_highlight(self.next_item)
+			self.next_item = self:item_next(self.next_item)
+			wav_for_the_next:and_then(play_once)
 		end)
 	end
 	self.playing_promise = self:ensure_wav_on_item(self.current_item, choose_name())
-	self.playing_promise.MARK2 = "the outer ensure_wav_on_item"
 	self.playing_promise:and_then(function()
 		play_once()
 	end)
